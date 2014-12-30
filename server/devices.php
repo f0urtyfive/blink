7c372c37e5e628a95df15b39d7262742b594453b
Matt Mills <mmills@2bn.net>
2014-12-30 00:39:38 -0700

    Add support for update API calls for on/off and intensity

--- server/devices.php
+++ server/devices.php
@@ -29,18 +29,38 @@ require_once('config.php');
 				new_div += "<div class='device'>";
 				new_div += " <span class='device name'>"+wink_name+"</span>";
 				new_div += " <br />";
-				new_div += " <input "+checked+" data-toggle='toggle' type='checkbox' class='device_toggle' id='device_toggle-"+wink_id+"'>";
-				new_div += " <input type='textbox' data-slider-value='"+intensity+"' data-slider-max=255 class='slider' id='intensity-"+wink_id+"'>";
+				new_div += " <input "+checked+" data-toggle='toggle' type='checkbox' class='device_toggle' id='device_toggle-"+wink_id+"' name='"+wink_id+"'>";
+				new_div += " <input data-slider-value='"+intensity+"' data-slider-max=255 class='slider' id='intensity-"+wink_id+"' name='"+wink_id+"'>";
 				new_div += "</div>";
 				new_div += "<hr>";
 
 
 				$('.body').append(new_div);
-				$('#device_toggle-'+wink_id).bootstrapToggle();
-				$('#intensity-'+wink_id).slider();
+				$('#device_toggle-'+wink_id).bootstrapToggle().change(function() {
+					var checked = "OFF";
+					if($(this).prop('checked') == true){
+						checked = "ON";
+					}
+					wink_update_on_off($(this).prop('name'), checked);
+					
+				});
+				$('#intensity-'+wink_id).slider().on('slideStop', function(ev){
+					wink_update_intensity(ev.target.name, ev.value);
+				});
 			}
 		});
 	});
+	function wink_update(device_id, attr_id, value){
+		$.post( api_url+'/commands/index.php' , JSON.stringify([{action: "update", id: device_id, updates: [{id:attr_id, value: value}] }]) , function(data){ } );
+	}
+
+	function wink_update_intensity(device_id, value){
+		wink_update(device_id, 2, value);
+	}
+	function wink_update_on_off(device_id, value){
+		wink_update(device_id, 1, value);
+	}
+
 </script>
 </head>
 <body>
